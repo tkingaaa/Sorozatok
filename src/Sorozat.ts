76329a97f8f3b863060fc06606b261434259de86
tkingaaa <tokzskinga@gmail.com>
2021-04-16 09:52:57 +0200

    Sorozat osztály elkészítése

---
+++ src/Sorozat.ts
@@ -0,0 +1,20 @@
+export default class Sorozat{
+    private _datum: string;
+    private _cim: string;
+    private _resz: string;
+    private _hossz: number;
+    private _latta: boolean;
+
+    constructor(adatok: string[]){
+        const a: string[] = adatok;
+        this._datum = a[0];
+        this._cim = a[1];
+        this._resz = a[2];
+        this._hossz = parseInt(a[3]);
+        if (a[4] == '0') {
+            this._latta=true;
+        } else {
+            this._latta = false;
+        }
+    }
+}
